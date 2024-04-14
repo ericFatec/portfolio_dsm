aef8e72e539af6a25a899c120443470ec43ce6ed
peaceFrog <eric.paula@fatec.sp.gov.br>
2024-04-13 21:26:28 -0300

    Added proper content to index page, created contact page and adjusted navbar and app.py to properly show the current page being accessed

--- src/app.py
+++ src/app.py
@@ -4,7 +4,11 @@ app = Flask("__name__")
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', current_page='home')
+
+@app.route("/contato")
+def contact():
+    return render_template('contato.html', current_page='contato')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
